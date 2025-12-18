edf2ce38e4e208dba5a22bad4b09cfb976788a9c
Filip Grala <filip.grala.1602@gmail.com>
2025-12-18 13:00:26 +0100

    poprawiono lokalizacje prac

--- PART_1/unicafe/src/App.jsx
+++ PART_1/unicafe/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Komenda1 = () => {
-  return console.log("Console test...\nConsole goes brrrr!"); 
+  return console.log("Console test...\nConsole!"); 
 };
 
 const Komenda2 = () => {
@@ -47,37 +47,9 @@ const App = () => {
   Komenda1();
   Komenda2();
 
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent accounts for the other 90 percent.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place.',
-    'Programming without console.log is like a doctor refusing x-rays.',
-    'The only way to go fast is to go well.'
-  ];
-
-  const [selected, setSelected] = useState(0);
-
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const handleVote = () => {
-    const copy = [...votes];
-    copy[selected] += 1;
-    setVotes(copy);
-  };
-
-  const handleNext = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  const highest = Math.max(...votes);
-  const highestIndex = votes.indexOf(highest);
 
   return (
     <div>
@@ -88,19 +60,6 @@ const App = () => {
 
       <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
-
-      <hr size="10" color="black"/>
-
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-
-      <Button onClick={handleVote} text="vote" />
-      <Button onClick={handleNext} text="next anecdote" />
-
-      <h2>Anecdote with most votes</h2>
-      <p>{anecdotes[highestIndex]}</p>
-      <p>has {highest} votes</p>
     </div>
   );
 };
