ac606a307a20a7b967d0a5e5970f1e9aed7f308f
Filip Grala <filip.grala.1602@gmail.com>
2026-01-15 12:19:20 +0100

    Skonczono exercise 2.1 Part 2 course information

--- PART_2/course_information/src/App.jsx
+++ PART_2/course_information/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-const Header = ({ courseName }) => {
-  return <h1>{courseName}</h1>;
-};
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
 
 const Part = ({ name, exercises }) => (
   <p>
@@ -12,43 +10,42 @@ const Part = ({ name, exercises }) => (
 
 const Content = ({ parts }) => (
   <div>
-    {parts.map((part, index) => (
-      <Part key={index} name={part.name} exercises={part.exercises} />
+    {parts.map((part) => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
     ))}
   </div>
 );
 
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
+const Course = ({ course }) => (
+  <div>
+    <Header courseName={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10,
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7,
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
