b7951133f0821507e44e81dad75707bad1244d71
Filip Grala <filip.grala.1602@gmail.com>
2026-01-22 13:34:49 +0100

    Skonczono exercise 2.6 Part 2 phonebook

---
+++ PART_2/phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = () => {
+    if (newName.trim() === '') {
+      console.log("Name can't be empty")
+      return
+    }
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    console.log(`Added: ${newName}`)
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button onClick={addPerson}>add</button>
+        </div>
+      </div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
