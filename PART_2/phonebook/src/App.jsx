941ea1203ca14fc68da9de6be17cc55001891dfa
Filip Grala <filip.grala.1602@gmail.com>
2026-01-29 13:39:00 +0100

    Skonczono exercise 2.10 Part 2 phonebook

--- PART_2/phonebook/src/App.jsx
+++ PART_2/phonebook/src/App.jsx
@@ -68,7 +68,7 @@ const App = () => {
       window.alert(`"${newName}" is already added to phonebook`);
       return
     }
-    const newPerson = { name: newName, number: newNumber, id: Date.now() }
+    const newPerson = { name: newName, number: newNumber, id: persons.length + 1 }
     setPersons(persons.concat(newPerson))
     setNewName('')
     setNewNumber('')
