16902ef6fc0ebbc27a371e59ef0dd40c3cbaf423
Filip Grala <filip.grala.1602@gmail.com>
2026-05-22 10:46:51 +0200

    Skonczono zadanie 9 Part 3B - 3.9

--- PART_3/phonebook/index.mjs
+++ PART_3/phonebook/index.mjs
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(express.json())
+app.use(cors())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -43,7 +45,6 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'number is required' })
   }
 
-  // Check if name already exists
   if (persons.some(p => p.name === name)) {
     return res.status(409).json({ error: 'name must be unique' })
   }
